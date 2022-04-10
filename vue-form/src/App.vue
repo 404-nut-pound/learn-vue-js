4fb46824be5f2bdb2c52598447e6ae987c3fd0cc
Skynet <souls4523@gmail.com>
2022-04-11 01:15:36 +0900

    vue-cli로 회원가입 폼 작성

---
+++ vue-form/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <form v-on:submit="submitForm">
+    <div>
+      <label for="username">ID: </label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW: </label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <button type="submit">LOGIN</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm: function (event) {
+      event.preventDefault();
+
+      console.log(this.username, this.password);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
